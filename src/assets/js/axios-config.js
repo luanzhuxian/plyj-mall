82867a634639c338cb44111546e7c5385122e681
Tesla <547249523@qq.com>
2019-05-23 11:20:09 +0800

    fix: 修改刷新token

--- src/assets/js/axios-config.js
+++ src/assets/js/axios-config.js
@@ -26,6 +26,7 @@ function reqError (error) {
 
 async function response (response) {
   const data = response.data
+  console.log(response)
   const config = response.config
   if (data.status !== 200) {
     let msg = data.message
@@ -51,7 +52,7 @@ async function response (response) {
     //   return
     // }
     // 接口报‘登录信息失效，退出登录，并重新发起登录
-    if (response.path !== '/apis/v1/account/account/info') {
+    if (response.config.url !== '/apis/v1/account/account/info') {
       store.dispatch(REFRESH_TOKEN)
     }
     return Promise.reject(new Error(msg))
