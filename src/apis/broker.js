d749e4263f77e276fdb2616fba3539bc1df28527
lzx <luanzhuxian@hotmail.com>
2019-07-03 17:52:10 +0800

    fix: 修改分类helper专区接口地址

--- src/apis/broker.js
+++ src/apis/broker.js
@@ -4,4 +4,4 @@ import axios from 'axios'
  * 获取经纪人分享所有商品
  * @param params
  */
-export const getActivityProduct = (params) => axios({ method: 'get', url: `/apis/v1/product/public/page/agent`, params })
+export const getActivityProduct = (params) => axios({ method: 'get', url: '/apis/v1/product/page/agent', params })
