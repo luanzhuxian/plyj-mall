35504046a9d10b4058510c621a15a6bd06b26ae1
lzx <luanzhuxian@hotmail.com>
2019-07-19 16:15:11 +0800

    fix: 修改首页

--- src/views/old-home/Template-New.vue
+++ src/views/old-home/Template-New.vue
@@ -1,5 +1,5 @@
 <template lang="html">
-  <div :class="$style.templateOld">
+  <div :class="$style.templateNew">
     <div :class="$style.top">
       <top-text :title="mallName + ' 欢迎您'" :tip="date" />
     </div>
@@ -33,6 +33,15 @@
       </div>
       <swiper :class="$style.swiper" :options="swiperOptionGift">
         <swiper-slide v-for="(item, i) of data['MODULEA'].values" :key="i">
+          <!-- <category-item
+            tag="div"
+            size="mini"
+            :img="item.goodsInfo.productMainImage + '?x-oss-process=style/thum-small'"
+            :is-active="item.agentProduct"
+            :product-id="item.goodsInfo.id"
+            :product-name="item.goodsInfo.productName"
+            :price="item.goodsInfo.productSkuModels.length && item.goodsInfo.productSkuModels[0].price"
+          /> -->
           <category-item
             tag="div"
             size="mini"
@@ -52,7 +61,7 @@
 
     <div :class="$style.activity" v-if="data['MODULEB'].values.length">
       <router-link
-        :class="$style.img"
+        :class="$style.imgWrapper"
         v-if="data['MODULEB'].values[0].type && data['MODULEB'].values[0].value"
         tag="img"
         :src="data['MODULEB'].values[0].image"
@@ -65,8 +74,8 @@
       >
     </div>
 
-    <div :class="$style.products" v-if="data['MODULEC'].values.length">
-      <swiper :options="swiperOptionProd">
+    <div :class="$style.products" v-if="data['MODULEC'].length">
+      <swiper :options="swiperOptionProd" style="overflow: visible">
         <swiper-slide v-for="(item, i) of data['MODULEC'].values" :key="i">
           <div :class="$style.wrapper">
             <!-- <category-item
@@ -95,8 +104,8 @@
 
     <div :class="$style.activity" v-if="data['MODULED'].values.length">
       <router-link
-        :class="$style.image"
-        v-if="data['MODULED'].values[0].type && data['MODULED'].values[0].value"
+        :class="$style.imgWrapper"
+        v-if="data['MODULED'].values[0].linkType && data['MODULED'].values[0].link"
         tag="img"
         :src="data['MODULED'].values[0].image"
         :to="getRouteLink(data['MODULED'].values[0])"
@@ -109,7 +118,7 @@
     </div>
 
     <div :class="$style.products" v-if="data['MODULEE'].values.length">
-      <swiper :options="swiperOptionProd">
+      <swiper :options="swiperOptionProd" style="overflow: visible">
         <swiper-slide v-for="(item, i) of data['MODULEE'].values" :key="i">
           <div :class="$style.wrapper">
             <category-item
@@ -130,21 +139,14 @@
 </template>
 
 <script>
-import moment from 'moment'
 import { swiper, swiperSlide } from 'vue-awesome-swiper'
-import TopText from '../../components/Top-Text.vue'
-import CategoryItem from './components/CategoryItem.vue'
-import YouLike from './components/YouLike.vue'
 import { mapGetters } from 'vuex'
 
 export default {
-  name: 'TemplateOld',
+  name: 'TemplateNew',
   components: {
     swiper,
-    swiperSlide,
-    TopText,
-    CategoryItem,
-    YouLike
+    swiperSlide
   },
   props: {
     data: {
@@ -156,7 +158,7 @@ export default {
   },
   data () {
     return {
-      swiperOptionBanner: {
+      swiperOption: {
         slidesPerView: 'auto',
         centeredSlides: true,
         autoplay: true,
@@ -165,114 +167,32 @@ export default {
           el: '.banner-pagination',
           clickable: true
         }
-      },
-      swiperOptionGift: {
-        slidesPerView: 3,
-        pagination: {
-          el: '.good-gift-pagination'
-        }
-      },
-      swiperOptionProd: {
-        slidesPerView: 2.5,
-        slidesPerGroup: 3
-      },
-      date: moment().format('MM月DD日 dddd')
+      }
     }
   },
   computed: {
     ...mapGetters(['agencyCode', 'agentUser', 'mallName', 'userId'])
   },
+  created () {},
   methods: {
-    getRouteLink ({ type, value }) {
-      const name = type === 1 ? 'Classify' : 'Lesson'
-      const id = (type === 1 && value === '全部商品') ? '' : value
-      return {
-        name,
-        params: {
-          ...(type === 1 ? { optionId: id } : null),
-          ...(type === 2 ? { productId: id } : null),
-          ...(type === 2 && this.agentUser ? { brokerId: this.userId } : null) // 如果当前用户是helper，尝试获取该商品的brokerId
-        }
-      }
-    }
+    // getRouteLink ({ type, value }) {
+    //   const name = type === 1 ? 'Classify' : 'Lesson'
+    //   const id = (type === 1 && value === '全部商品') ? '' : value
+    //   return {
+    //     name,
+    //     params: {
+    //       ...(type === 1 ? { optionId: id } : null),
+    //       ...(type === 2 ? { productId: id } : null),
+    //       ...(type === 2 && this.agentUser ? { brokerId: this.userId } : null) // 如果当前用户是helper，尝试获取该商品的brokerId
+    //     }
+    //   }
+    // }
   }
 }
 </script>
 
 <style module lang="scss">
-.template-old {
-  .top {
-    padding: 30px 40px;
-  }
-  .banner {
-    height: 354px;
-    .img {
-      width: 100%;
-      height: 100%;
-      object-fit: cover;
-      background-color: #fff;
-    }
-  }
-
-  .gift {
-    width: 686px;
-    padding: 40px 30px 30px 30px;
-    margin: 48px auto 80px;
-    background-color: #fff;
-    border-radius: $--radius1;
-    box-sizing: border-box;
-    .head {
-      text-align: center;
-      margin-bottom: 40px;
-      .title {
-        font-size: 36px;
-        color: #000;
-        font-weight: bold;
-      }
-      p {
-        margin-top: 10px;
-        font-size: 24px;
-        color: #666;
-      }
-    }
-  }
-
-  .activity {
-    margin-top: 41px;
-    padding: 0 32px;
-    height: 324px;
-    .img {
-      width: 100%;
-      height: 100%;
-      border-radius: $--radius1;
-      overflow: hidden;
-      object-fit: cover;
-    }
-  }
-
-  .products {
-    margin-top: 28px;
-    margin-bottom: 40px;
-    padding: 0 32px;
-    .wrapper {
-      display: inline-flex;
-      align-items: center;
-      justify-content: center;
-      width: 248px;
-      padding: 28px 0;
-      background-color: #fff;
-      border-radius: $--radius1;
-      width: 100%;
-      margin-right: 20px;
-    }
-  }
-  .broker {
-    margin-top: 30px
-  }
-  .youLike {
-    padding: 0 32px;
-  }
-}
+  import './index.scss';
 </style>
 <style lang="scss">
   .swiper-slide {
