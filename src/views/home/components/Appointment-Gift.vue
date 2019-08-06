77a0a380f3aa7086859bc1cc26678d2e9f9064b2
Tesla <547249523@qq.com>
2019-08-06 15:19:34 +0800

    feat: 首页预约

--- src/views/home/components/Appointment-Gift.vue
+++ src/views/home/components/Appointment-Gift.vue
@@ -1,6 +1,6 @@
 <template>
   <div :class="$style.appointment">
-    <div :class="$style.appointmentGift">
+    <div :class="$style.appointmentGift" @click="showPop = true">
       <pl-svg :class="$style.giftIcon" name="gift" />
       <div :class="$style.right">
         <pl-svg name="yuyue2" />
@@ -45,6 +45,34 @@
         </ul>
       </div>
     </div>
+
+    <pl-popup :show.sync="showPop" title="在线预约">
+      <div :class="$style.popContent">
+        <pl-svg :class="$style.giftIcon" name="gift" />
+        <div :class="$style.right">
+          <pl-svg name="yuyue2" />
+        </div>
+        <ul :class="$style.left">
+          <li>免费试听, 2小时外教课</li>
+          <li>到店礼精美礼品+外教口语体</li>
+          <li>到店礼精美礼品+外教口语体</li>
+          <li>到店礼精美礼品+外教口语体</li>
+        </ul>
+      </div>
+      <form :class="$style.popForm">
+        <div class="fz-26 gray-3">预约后您的私人顾问将会电话联系您</div>
+        <input value="123" type="text" placeholder="请输入预约手机">
+        <div :class="$style.tip">
+          <pl-svg name="safe" />
+          <span>无强行推销</span>
+          <pl-svg name="safe" />
+          <span>不泄露用户手机号码</span>
+        </div>
+        <pl-button size="large" type="warning">
+          确认预约
+        </pl-button>
+      </form>
+    </pl-popup>
   </div>
 </template>
 
@@ -52,6 +80,11 @@
 import { mapGetters } from 'vuex'
 export default {
   name: 'AppointmentGift',
+  data () {
+    return {
+      showPop: false
+    }
+  },
   computed: {
     ...mapGetters(['logoUrl'])
   }
@@ -70,12 +103,6 @@ export default {
       padding: 20px 24px;
       background: linear-gradient(90deg, #FEEFD1, #E7D79C);
       border-radius: 20px;
-      .gift-icon {
-        position: absolute;
-        top: 0;
-        left: 0;
-        width: 80px;
-      }
       > .right {
         position: relative;
         display: inline-flex;
@@ -187,6 +214,104 @@ export default {
             border-radius: 25px;
           }
         }
+      }
+    }
+  }
+  .gift-icon {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 80px;
+  }
+  .pop-title {
+    margin-top: 10px;
+    line-height: 80px;
+    font-size: 30px;
+    text-align: center;
+  }
+  .pop-content {
+    position: relative;
+    display: flex;
+    margin-bottom: 26px;
+    align-items: center;
+    padding: 32px 64px;
+    background-color: #feefd1;
+    .gift-icon {
+      width: 100px;
+    }
+    > .right {
+      position: relative;
+      display: inline-flex;
+      align-items: center;
+      width: 94px;
+      height: 34px;
+      padding-right: 20px;
+      &:after {
+        position: absolute;
+        right: 0;
+        content: '';
+        width: 2px;
+        height: 100%;
+        background: linear-gradient(180deg, #F3D488, #EAB665);
+      }
+      > svg {
+        width: 94px;
+      }
+    }
+    > .left {
+      position: relative;
+      display: inline-flex;
+      flex-direction: column;
+      justify-content: space-between;
+      flex: 1;
+      height: 100%;
+      padding-left: 20px;
+      font-size: 28px;
+      color: #AB8F58;
+      > li {
+        position: relative;
+        display: flex;
+        align-items: center;
+        margin-bottom: 12px;
+        &:nth-last-of-type(1) {
+          margin-bottom: 0;
+        }
+        &:before {
+          position: relative;
+          display: inline-block;
+          content: '';
+          width: 12px;
+          height: 12px;
+          margin-right: 12px;
+          background-color: #AB8F58;
+          border-radius: 6px;
+        }
+      }
+    }
+  }
+  .pop-form {
+    padding: 0 24px 20px;
+    > input {
+      width: 100%;
+      height: 80px;
+      margin-top: 26px;
+      margin-bottom: 20px;
+      padding: 0 20px;
+      border: 1px solid #ccc;
+      font-size: 32px;
+      border-radius: 20px;
+      background-color: #F4F5F9;
+    }
+    > .tip {
+      font-size: 24px;
+      margin-bottom: 44px;
+      > svg {
+        width: 20px;
+        margin-right: 15px;
+        vertical-align: -2px;
+      }
+      > span {
+        margin-right: 15px;
       }
     }
   }
