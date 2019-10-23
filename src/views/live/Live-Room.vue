3d9699a15035b2afe8be93bf768fd1696c958a58
Tesla <547249523@qq.com>
2019-10-23 13:46:19 +0800

    fix: 修改聊天

--- src/views/live/Live-Room.vue
+++ src/views/live/Live-Room.vue
@@ -162,6 +162,7 @@
 </template>
 
 <script>
+/* eslint-disable */
 import 'vue-slider-component/theme/default.css'
 import { mapGetters } from 'vuex'
 import CouponItem from '../../components/item/Coupon-Item.vue'
@@ -320,11 +321,6 @@ export default {
           case 'SPEAK':
             if (this.userName !== user.nick) {
               let message = mData.values.join(',')
-              // let emo = /\[.+\]/.exec(message)
-              // console.log(message)
-              // for (let e of emo) {
-              //   console.log(e)
-              // }
               this.chatRecords.push({
                 message,
                 name: user.nick,
@@ -345,28 +341,15 @@ export default {
         this.scrollBottom()
       }
     },
+    /* 聊天发送消息 */
     async sendMessage (message) {
       try {
-        let { channelId, liveAppId, avatar, userName } = this
-        let timestamp = Date.now()
-        let result = await sign({
-          signMsg: `appId${liveAppId}channelId${channelId}msg${message}nickName${userName}pic${avatar}timestamp${timestamp}`,
+        let { channelId } = this
+        this.socket.emit('message', JSON.stringify({
+          EVENT: 'SPEAK',
+          values: [message], // 发言内容
           roomId: channelId
-        })
-        let messageConfig = {
-          appId: liveAppId,
-          timestamp,
-          channelId,
-          msg: message,
-          pic: avatar,
-          nickName: userName,
-          sign: result
-          // adminIndex
-          // actor
-          // freeReview
-        }
-        let data = await sendMessage(messageConfig)
-        return data
+        }))
       } catch (e) {
         throw e
       }
