2b5ef1cdd5be7cabaf297bdb1c2e0603af90b4b6
lzx <luanzhuxian@hotmail.com>
2019-11-18 17:13:50 +0800

    fix: 修改分享功能

--- src/assets/js/router-guard.js
+++ src/assets/js/router-guard.js
@@ -1,5 +1,5 @@
 import { router } from '../../router'
-// import { isIOS } from './util'
+import { isIOS } from './util'
 // import store from '../../store'
 // 小金库相关页面，非helper不能访问
 // const notAllowedCoffer = [
@@ -11,27 +11,27 @@ import { router } from '../../router'
 //   'AccumulatedRebate',
 //   'RebateList'
 // ]
-// const map = {
-//   InviteNewcomers: '/yx/',
-//   Newcomers: '/newcomers/',
-//   LiveRoom: '/lived/room',
-//   Lesson: '/detail/lesson/'
-// }
+const map = {
+  InviteNewcomers: '/yx/',
+  Newcomers: '/newcomers/',
+  LiveRoom: '/lived/room',
+  Lesson: '/detail/lesson/'
+}
 
 export const beforeEach = function (to, from, next) {
-  // if (!isIOS()) return next()
-  // if (!~Object.keys(map).indexOf(to.name)) return next()
-  // const { pathname } = location
-  // const mallUrl = pathname.split('/')[1]
-  // for (let [key, val] of Object.entries(map)) {
-  //   if (to.name !== key) continue
-  //   if (!pathname.includes(val)) {
-  //     location.assign(`/${mallUrl}${to.fullPath}`)
-  //   } else {
-  next()
-  //   }
-  //   break
-  // }
+  if (!isIOS()) return next()
+  if (!~Object.keys(map).indexOf(to.name)) return next()
+  const { pathname } = location
+  const mallUrl = pathname.split('/')[1]
+  for (let [key, val] of Object.entries(map)) {
+    if (to.name !== key) continue
+    if (!pathname.includes(val)) {
+      location.assign(`/${mallUrl}${to.fullPath}`)
+    } else {
+      next()
+    }
+    break
+  }
 }
 export const beforeResolve = function (to, from, next) {
   // console.log(store.getters.agentUser)
