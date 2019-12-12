8bdfb57fb87365b53ce4d7b6e263f5b9c70eaa4b
lzx <luanzhuxian@hotmail.com>
2019-12-12 18:01:18 +0800

    fix: 修改主会场预购

--- src/views/activity/mixin.js
+++ src/views/activity/mixin.js
@@ -40,6 +40,22 @@ export default {
       } else if (item.goodsInfo.activityInfo.status === 1) {
         item.goodsInfo.activityInfo.status = 3
       }
+    },
+    sub (arg1, arg2) {
+      var r1, r2, m, n
+      try {
+        r1 = arg1.toString().split('.')[1].length
+      } catch (e) {
+        r1 = 0
+      }
+      try {
+        r2 = arg2.toString().split('.')[1].length
+      } catch (e) {
+        r2 = 0
+      }
+      m = Math.pow(10, Math.max(r1, r2))
+      n = (r1 >= r2) ? r1 : r2
+      return ((arg1 * m - arg2 * m) / m).toFixed(n)
     }
   }
 }
