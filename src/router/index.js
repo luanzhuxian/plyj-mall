d9f2c8840c245a92cdb6d5b54e5be91ac28f72b0
Tesla <547249523@qq.com>
2019-12-23 19:37:50 +0800

    fix: 修改首页重定向错误

--- src/router/index.js
+++ src/router/index.js
@@ -68,7 +68,7 @@ Router.prototype.replace = function replace (location, onResolve, onReject) {
 export const routes = [
   {
     path: '/',
-    redirect: `/home/${Date.now()}`
+    redirect: `/home?noCache=${Date.now()}`
   },
   {
     path: '*',
