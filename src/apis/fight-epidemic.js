7a85d2338e49831711ab6983cedc90365b9f70aa
Leon <lileo126@126.com>
2020-02-04 09:08:01 +0800

    fix: 抗疫签到请求

--- src/apis/fight-epidemic.js
+++ src/apis/fight-epidemic.js
@@ -15,3 +15,21 @@ export const getBookActivity = () => axios.get('/apis/v1/epidemic/has_epidem_act
 export const signUp = data => axios.post('/apis/v1/epidemicActivity/signUp', data)
 // 报名数据
 export const signInfo = activityId => axios.get(`/apis/v1/epidemicActivity/querySignUpInfo/${activityId}`)
+
+// 疫情签到的活动信息
+export const getactivityInfo = activityId => axios.get(`/apis/v1/epidemic/epidem_activity/${activityId}`)
+
+// 我的签到信息
+export const getSignInInfo = activityId => axios.get(`/apis/v1/epidemic/my_sign_info/${activityId}`)
+
+// 抗疫签到
+export const signIn = ({ activityId, content }) => axios.post(`/apis/v1/epidemic/signin/${activityId}`, null, { params: { content } })
+
+// 抗疫弹幕
+export const getBarrage = activityId => axios.get(`/apis/v1/epidemic/new_signin_notices/${activityId}`)
+
+// 获取兑换礼品信息
+export const getGiftInfo = (activityId, userId) => axios.get(`/apis/v1/epidemicActivity/my_gift_info/${activityId}`, { params: { userId } })
+
+// 兑换礼品
+export const reedemGift = ({ activityId, userId, giftId }) => axios.get(`/apis/v1/epidemicActivity/reedemGift/${activityId}`, { params: { userId, giftId } })
