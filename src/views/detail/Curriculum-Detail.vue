f07f3e18e27072dfde6831733bab248252b31d77
Tesla <547249523@qq.com>
2020-02-25 18:52:04 +0800

    fix: 课程海报

--- src/views/detail/Curriculum-Detail.vue
+++ src/views/detail/Curriculum-Detail.vue
@@ -1,5 +1,11 @@
 <template>
   <div :class="$style.curriculum">
+    <!-- 海报按钮 -->
+    <div :class="$style.haibao">
+      <pl-svg :key="1" v-if="creating" name="icon-btn-loading" width="35" fill="#fff" class="rotate" />
+      <pl-svg :key="2" v-else name="icon-haibao" width="35" @click="createHaibao" />
+      <p>分享海报</p>
+    </div>
     <Banner :banners="banners" />
     <InfoBox>
       <div :class="$style.priceBox">
@@ -74,6 +80,19 @@
       </div>
     </div>
     <Contact :show.sync="showContact" />
+
+    <!-- 海报弹框 -->
+    <transition name="fade">
+      <div :class="$style.saveHaibao" v-if="showHaibao">
+        <div :class="$style.saveHaibaoContent">
+          <img :src="haibao" alt="">
+          <div :class="$style.saveButton">
+            长按识别或保存二维码，分享给朋友吧！
+          </div>
+          <pl-svg name="icon-close3" fill="#fff" width="30" @click="showHaibao = false;" />
+        </div>
+      </div>
+    </transition>
   </div>
 </template>
 
@@ -87,7 +106,9 @@ import Tags from '../../components/detail/Tags.vue'
 import Contact from '../../components/common/Contact.vue'
 import { getCourseDetail } from '../../apis/product'
 import share from '../../assets/js/wechat/wechat-share'
+import { generateQrcode, cutImageCenter, cutArcImage, loadImage, createText } from '../../assets/js/util'
 import { mapGetters } from 'vuex'
+const avatar = 'https://penglai-weimall.oss-cn-hangzhou.aliyuncs.com/static/default-avatar.png'
 export default {
   name: 'CurriculumDetail',
   components: {
@@ -107,7 +128,10 @@ export default {
       loading: false,
       tab: 2,
       studied: true,
-      showContact: false
+      showContact: false,
+      creating: false,
+      showHaibao: false,
+      haibao: ''
     }
   },
   props: {
@@ -121,7 +145,7 @@ export default {
     }
   },
   computed: {
-    ...mapGetters(['appId']),
+    ...mapGetters(['appId', 'userName', 'avatar']),
     // 0 全部，1 helper，2 会员，3 部分用户
     targetGroups () {
       return this.detail.targetGroups
@@ -174,6 +198,7 @@ export default {
           link: shareUrl,
           imgUrl: result.productMainImage
         })
+        this.haibaoImg = await loadImage(courseImg)
         return result
       } catch (e) {
         throw e
@@ -195,6 +220,91 @@ export default {
         await this.getDetail()
       } catch (e) {
         throw e
+      }
+    },
+    async createHaibao (type) {
+      if (this.loading) {
+        return
+      }
+      let img = this.haibaoImg
+      if (!img) {
+        this.$error('图片加载错误')
+        return
+      }
+      if (this.haibao) {
+        this.showHaibao = true
+        return
+      }
+      this.creating = true
+      // 截取头像
+      let lodedAvatar
+      try {
+        lodedAvatar = await loadImage(this.avatar)
+      } catch (e) {
+        lodedAvatar = await loadImage(avatar)
+      }
+      const arcAvatar = cutArcImage(lodedAvatar)
+      // 截取中间部分
+      img = cutImageCenter(img, 750 / 500)
+      let canvas = document.createElement('canvas')
+      canvas.width = 1120
+      canvas.height = 1346
+      let ctx = canvas.getContext('2d')
+      // 绘制头部
+      ctx.fillStyle = '#fff'
+      ctx.fillRect(0, 0, 1120, 192)
+      ctx.drawImage(arcAvatar, 32, 32, 128, 128)
+      ctx.font = 'bold 48px Microsoft YaHei UI'
+      ctx.fillStyle = '#000'
+      createText(ctx, 192, 120, this.userName, 68, 800, 1)
+      try {
+        // 二维码
+        let qrcode = await generateQrcode(300, this.shareUrl, 15, img, 10, 'canvas')
+        // 商品图片
+        ctx.drawImage(img, 0, 0, img.width, img.height, 0, 192, 1120, 746)
+        if (type !== 1 && this.preActivity === 2) {
+          ctx.fillStyle = '#FA4D2F'
+        } else {
+          ctx.fillStyle = '#fff'
+        }
+        ctx.fillRect(0, 938, 1120, 408)
+        ctx.drawImage(qrcode, 750, 978, 320, 320)
+        // 填充商品名称
+        // let str = this.detail.courseName
+        let line = (type !== 1 && this.preActivity === 2) ? 1 : 2
+        let price = this.detail.sellingPrice
+        let originalPrice = this.detail.originalPrice
+        ctx.textBaseline = 'top'
+        ctx.font = '56px Microsoft YaHei UI'
+        ctx.fillStyle = '#000'
+        // 填充价钱
+        createText(ctx, 48, 978, this.detail.courseName, 80, 620, line)
+        ctx.fillStyle = '#FE7700'
+        ctx.fillText('¥', 48, 1190 + (76 - 56) / 2)
+        ctx.font = 'bold 88px Microsoft YaHei UI'
+        createText(ctx, 96, 1170 + (104 - 88) / 2, String(price), 104)
+        // 绘制原价
+        if (originalPrice && originalPrice !== price) {
+          let priceWidth = ctx.measureText(price).width
+          ctx.fillStyle = '#999'
+          ctx.font = '56px Microsoft YaHei UI'
+          ctx.fillText(`¥${originalPrice}`, 96 + priceWidth + 44, 1190 + (80 - 56) / 2)
+          let originalPriceWidth = ctx.measureText(`¥${originalPrice}`).width
+          ctx.save()
+          // 设置删除线
+          ctx.strokeStyle = '#999'
+          ctx.beginPath()
+          ctx.lineWidth = '4'
+          ctx.moveTo(96 + priceWidth + 44, 1190 + (80 - 56) / 2 + 80 / 3)
+          ctx.lineTo(96 + priceWidth + 44 + originalPriceWidth, 1190 + (80 - 56) / 2 + 80 / 3)
+          ctx.stroke()
+        }
+        this.haibao = canvas.toDataURL('image/jpeg', 0.7)
+        this.showHaibao = true
+      } catch (e) {
+        throw e
+      } finally {
+        this.creating = false
       }
     }
   }
@@ -303,6 +413,72 @@ export default {
     }
     .has-studied {
       background-color: #F2B036;
+    }
+  }
+
+  .haibao {
+    position: fixed;
+    right: 0;
+    top: 20px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    width: 100px;
+    height: 84px;
+    font-size: 16px;
+    color: #fff;
+    background-color: rgba(0, 0, 0, .5);
+    z-index: 20;
+    border-top-left-radius: 10px;
+    border-bottom-left-radius: 10px;
+    > svg {
+      width: 33px;
+    }
+    > p {
+      margin-top: 4px;
+    }
+  }
+  .saveHaibao {
+    position: fixed;
+    top: 0;
+    left: 0;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, .7);
+    z-index: 10000;
+    .saveHaibaoContent {
+      position: relative;
+      display: flex;
+      flex-direction: column;
+      justify-content: flex-start;
+      width: max-content;
+      height: max-content;
+      > .saveButton {
+        width: 560px;
+        margin-top: -4px;
+        text-align: center;
+        line-height: 66px;
+        font-size: 28px;
+        color: #666;
+        background-color: #FBFBFB;
+      }
+      > img {
+        width: 560px;
+        object-fit: cover;
+      }
+      > svg {
+        position: absolute;
+        top: -64px;
+        right: 0;
+        width: 48px;
+        height: 48px;
+        color: #fff;
+      }
     }
   }
 </style>
