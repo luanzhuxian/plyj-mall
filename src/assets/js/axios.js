bddbf8251e9703c09f71f937b2ecee9d0b3a5695
Tesla <547249523@qq.com>
2020-03-06 13:03:12 +0800

    fix: 修改超时时间

--- src/assets/js/axios.js
+++ src/assets/js/axios.js
@@ -1,7 +1,7 @@
 import Axios from 'axios'
 
 export const axios = Axios.create({
-  timeout: 150000000000,
+  timeout: 15000,
   headers: {
     'Content-Type': 'application/json;charset=UTF-8'
   }
