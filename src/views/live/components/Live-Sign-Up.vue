116c39a29749b26ea95fa650330ee5600a185134
lishaobos <www.long126@qq.com>
2020-03-18 13:13:05 +0800

    fix: 报名校验

--- src/views/live/components/Live-Sign-Up.vue
+++ src/views/live/components/Live-Sign-Up.vue
@@ -43,6 +43,14 @@ import { isPhone, checkLength } from './../../../assets/js/validate'
 import { liveSignUp } from './../../../apis/live'
 import Popup from './../../../components/penglai-ui/Popup'
 import CitySelector from './../../../components/common/City-Selector'
+// 验证是否中英文数字
+const signValidator = value => {
+    const reg = /^[a-zA-Z_\u4e00-\u9fa5_1-9]{1,60}$/
+    if (!reg.test(value)) {
+        return false
+    }
+    return true
+}
 export default {
     name: 'LiveSignUp',
     components: {
@@ -83,7 +91,8 @@ export default {
             rulesTemplate: {
                 signName: [
                     { required: true, message: '请输入姓名' },
-                    { validator: checkLength(20), message: '长度在20字以内' }
+                    { validator: checkLength(20), message: '长度在20字以内' },
+                    { validator: signValidator, message: '请输入正确的姓名格式' }
                 ],
                 signTelphone: [
                     { required: true, message: '请输入手机号' },
@@ -91,7 +100,8 @@ export default {
                 ],
                 signGrade: [
                     { required: true, message: '请输入年级' },
-                    { validator: checkLength(20), message: '长度在20字以内' }
+                    { validator: checkLength(20), message: '长度在20字以内' },
+                    { validator: signValidator, message: '请输入正确的年级格式' }
                 ],
                 signRegion: [
                     { required: true, message: '请选择区域' },
@@ -99,7 +109,8 @@ export default {
                 ],
                 signTitle: [
                     { required: true, message: `请输入${ this.info.isHaveCustomer || '' }` },
-                    { validator: checkLength(20), message: '长度在20字以内' }
+                    { validator: checkLength(20), message: '长度在20字以内' },
+                    { validator: signValidator, message: `请输入正确的${ this.info.isHaveCustomer || '' }格式` }
                 ]
             }
         }
@@ -156,7 +167,9 @@ export default {
                 this.rules = {}
                 if (this.info.isHaveCustomer) {
                     this.rules.signTitle = [
-                        { required: true, message: `请输入${ this.info.isHaveCustomer || '' }` }
+                        { required: true, message: `请输入${ this.info.isHaveCustomer || '' }` },
+                        { validator: checkLength(20), message: '长度在20字以内' },
+                        { validator: signValidator, message: `请输入正确的${ this.info.isHaveCustomer || '' }格式` }
                     ]
                 }
                 for (let i = 0; i < keys.length; i++) {
@@ -173,32 +186,32 @@ export default {
 
 <style lang='scss' module>
 
-  .content {
+.content {
     box-sizing: border-box;
     padding: 58px 38px 14px 38px;
     > .form {
-      box-sizing: border-box;
-      padding: 0 54px;
-      > .title {
-        margin-bottom: 10px;
-        font-size: 26px;
-        color: #333333;
-      }
-      > label {
-        // width: 486px;
-      }
+        box-sizing: border-box;
+        padding: 0 54px;
+        > .title {
+            margin-bottom: 10px;
+            font-size: 26px;
+            color: #333;
+        }
+        > label {
+            // width: 486px;
+        }
     }
     > .button {
-      width: 100%;
-      margin-top: 40px;
-      border-radius: 10px;
-      line-height: 80px;
-      text-align: center;
-      font-size: 28px;
-      font-weight: 500;
-      color: #FFFFFF;
-      background-color: #F2B036;
+        width: 100%;
+        margin-top: 40px;
+        border-radius: 10px;
+        line-height: 80px;
+        text-align: center;
+        font-size: 28px;
+        font-weight: 500;
+        color: #fff;
+        background-color: #f2b036;
     }
-  }
+}
 
 </style>
