692685568f39ed6c83abf7b614d627db70cf6a06
Tesla <547249523@qq.com>
2020-03-27 17:01:36 +0800

    fix: 修改联系方式组件，新增联系人姓名
    Signed-off-by: Tesla <547249523@qq.com>

--- src/components/common/Contact.vue
+++ src/components/common/Contact.vue
@@ -6,8 +6,12 @@
                     v-for="(item, i) of servicePhoneModels"
                     :key="i"
                     :href="'tel:' + item.contactWay"
-                    v-text="item.contactWay"
-                />
+                >
+                    <span>
+                        {{ item.contactUserName }}：
+                    </span>
+                    <span v-text="item.contactWay" />
+                </a>
             </div>
             <button :class="$style.cancel" @click="close">
                 取消
@@ -18,6 +22,7 @@
 
 <script>
 import { mapGetters } from 'vuex'
+
 export default {
     name: 'Contact',
     props: {
@@ -35,46 +40,54 @@ export default {
 </script>
 
 <style module lang="scss">
-  .contact {
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    z-index: 12;
-    background-color: rgba(0, 0, 0, .5);
-  }
-  .list {
-    position: absolute;
-    left: 50%;
-    bottom: 132px;
-    transform: translateX(-50%);
-    width: 702px;
-    border-radius: 20px;
-    overflow: hidden;
-    > a {
-      display: block;
-      font-size: 36px;
-      color: #598BF8;
-      text-align: center;
-      line-height: 100px;
-      border-bottom: 1px solid #e7e7e7;
-      background-color: #fff;
-      &:nth-last-of-type(1) {
-        border-bottom: none;
-      }
+    .contact {
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        z-index: 12;
+        background-color: rgba(0, 0, 0, .5);
     }
-  }
-  .cancel {
-    position: absolute;
-    left: 50%;
-    bottom: 16px;
-    transform: translateX(-50%);
-    width: 702px !important;
-    line-height: 100px;
-    font-size: 36px;
-    color: #598BF8;
-    border-radius: 20px;
-    background-color: #fff;
-  }
+
+    .list {
+        position: absolute;
+        left: 50%;
+        bottom: 132px;
+        transform: translateX(-50%);
+        width: 702px;
+        border-radius: 20px;
+        overflow: hidden;
+        > a {
+            display: block;
+            padding: 0 24px;
+            font-size: 30px;
+            color: #598BF8;
+            text-align: center;
+            line-height: 80px;
+            border-bottom: 1px solid #e7e7e7;
+            background-color: #fff;
+            @include elps();
+
+            &:nth-last-of-type(1) {
+                border-bottom: none;
+            }
+            > .name {
+                color: #999;
+            }
+        }
+    }
+
+    .cancel {
+        position: absolute;
+        left: 50%;
+        bottom: 16px;
+        transform: translateX(-50%);
+        width: 702px !important;
+        line-height: 100px;
+        font-size: 36px;
+        color: #598BF8;
+        border-radius: 20px;
+        background-color: #fff;
+    }
 </style>
