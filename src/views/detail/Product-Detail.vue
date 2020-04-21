93b22751d0eea3f9a8824d4421514da95ee1b4e9
Tesla <547249523@qq.com>
2020-04-21 13:10:00 +0800

    fix: 修改图标颜色

--- src/views/detail/Product-Detail.vue
+++ src/views/detail/Product-Detail.vue
@@ -12,7 +12,7 @@
                 <!-- 海报按钮 -->
                 <div :class="$style.haibao">
                     <pl-svg :key="1" v-show="creating" name="icon-btn-loading" width="35" fill="#fff" class="rotate" />
-                    <pl-svg :key="2" v-show="!creating" name="icon-poster-512b1" width="35" @click="createHaibao(activeProduct)" />
+                    <pl-svg :key="2" v-show="!creating" name="icon-poster-512b1" fill="#fff" width="35" @click="createHaibao(activeProduct)" />
                     <p>分享海报</p>
                 </div>
                 <!-- 商品banner -->
