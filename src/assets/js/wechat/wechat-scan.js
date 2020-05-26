e518779da8ad70c83da25f7ec6a5158ebd93f03b
EchoPa <cuilulu4043@126.com>
2020-05-26 17:10:15 +0800

    feat: (兑换码) 扫码回调,等会儿要删除

--- src/assets/js/wechat/wechat-scan.js
+++ src/assets/js/wechat/wechat-scan.js
@@ -4,11 +4,16 @@ const WX = window.wx
 /* eslint-disable */
 export default async function scan ({ appId }) {
     try {
+        console.log(11111)
         const { result: jsApi } = await getJSApi(appId)
+        console.log(2222)
         const nonceStr = randomString()
+        console.log(3333)
         const timestamp = Number.parseInt(new Date().getTime() / 1000)
+        console.log(4444)
         const sign = `jsapi_ticket=${ jsApi }&noncestr=${ nonceStr }&timestamp=${ timestamp }&url=${ location.href }`
         const signature = new JsSHE(sign, 'TEXT').getHash('SHA-1', 'HEX')
+        console.log(5555)
         const config = {
             // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
             debug: false,
@@ -25,6 +30,7 @@ export default async function scan ({ appId }) {
                 'scanQRCode'
             ]
         }
+        console.log(6666)
         WX.config(config)
         WX.ready(() => {
             console.warn('扫描配置成功')
