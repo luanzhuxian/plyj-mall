94c5a37406dfc7703441eae1ed29a2821d1269cd
Tesla <547249523@qq.com>
2020-06-12 15:54:47 +0800

    fix: 优化图标组件
    Signed-off-by: Tesla <547249523@qq.com>

--- src/components/common/Pl-Svg.vue
+++ src/components/common/Pl-Svg.vue
@@ -24,6 +24,7 @@
 
 <script>
 import { promise } from '../../assets/js/util'
+
 export default {
     name: 'PlSvg',
     props: {
@@ -103,76 +104,77 @@ export default {
         },
         async name (val) {
             /* eslint-disable */
-            if (this.fill && !this.clonedSvg[`${ val }_${ this._uid }`]) {
-                this.setFill(this.fill)
-            } else {
-                this.tempName = this.tempName.replace(this.tempName.split('_')[0], val)
+                if (this.fill && !this.clonedSvg[`${val}_${this._uid}`]) {
+                    this.setFill(this.fill)
+                } else {
+                    this.tempName = this.tempName.replace(this.tempName.split('_')[0], val)
+                }
             }
-        }
-    },
-    methods: {
-        clickHandler (e) {
-            this.$emit('click', e)
         },
-        hoverHandler (e) {
-            this.$emit('hover', e)
-        },
-        async setFill (fill) {
-            if (this.type !== 'svg') return
-            /**
-         * 由于使用的是svg精灵，所以
-         * 要使得fill属性生效，必须保证不存在行内fill属性 或者 修改行内fill属性
-         * 如果直接修改行内属性，会改变全局所有的svg颜色
-         * 所以，先复制一份svg，然后修改这个副本的颜色，使用完之后，再删除这个副本，避免副本越来越多
-         */
-            // _uid可以作为每个组件唯一的标识，用来作为图标副本的id，再合适不过了
-            const uid = this._uid
-            const svg = document.querySelector(`#${ this.name }`)
-            if (!svg) {
-                console.error(`svg ${ this.name } 不存在`)
-                return
-            }
-            this.tempName = `${ this.name }_${ uid }`
-            const clonedSvg = this.clonedSvg[this.tempName] || svg.cloneNode(true)
-            await promise.timeout(100)
-            svg.parentNode.appendChild(clonedSvg)
-            this.svgParent = svg.parentNode
-            this.clonedSvg[this.tempName] = clonedSvg
-
-            // 给克隆后的图标填充颜色
-            try {
-                const fills = clonedSvg.querySelectorAll('[fill]')
-                const colors = clonedSvg.querySelectorAll('[color]')
+        methods: {
+            clickHandler(e) {
+                this.$emit('click', e)
+            },
+            hoverHandler(e) {
+                this.$emit('hover', e)
+            },
+            async setFill(fill) {
+                if (this.type !== 'svg') return
+                /**
+                 * 由于使用的是svg精灵，所以
+                 * 要使得fill属性生效，必须保证不存在行内fill属性 或者 修改行内fill属性
+                 * 如果直接修改行内属性，会改变全局所有的svg颜色
+                 * 所以，先复制一份svg，然后修改这个副本的颜色，使用完之后，再删除这个副本，避免副本越来越多
+                 */
+                // _uid可以作为每个组件唯一的标识，用来作为图标副本的id，再合适不过了
+                const uid = this._uid
+                const svg = document.querySelector(`#${this.name}`)
+                if (!svg) {
+                    console.error(`svg ${this.name} 不存在`)
+                    return
+                }
+                this.tempName = `${this.name}_${uid}`
+                const clonedSvg = this.clonedSvg[this.tempName] || svg.cloneNode(true)
                 clonedSvg.setAttribute('id', this.tempName)
-                clonedSvg.setAttribute('fill', fill)
-                for (const fillEle of fills) {
-                    if (fillEle.getAttribute('fill') !== 'none') {
-                        fillEle.setAttribute('fill', fill)
+                await promise.timeout(100)
+                svg.parentNode.appendChild(clonedSvg)
+                this.svgParent = svg.parentNode
+                this.clonedSvg[this.tempName] = clonedSvg
+
+                // 给克隆后的图标填充颜色
+                try {
+                    const fills = clonedSvg.querySelectorAll('[fill]')
+                    const colors = clonedSvg.querySelectorAll('[color]')
+                    clonedSvg.setAttribute('fill', fill)
+                    for (const fillEle of fills) {
+                        if (fillEle.getAttribute('fill') !== 'none') {
+                            fillEle.setAttribute('fill', fill)
+                        }
+                    }
+                    for (const color of colors) {
+                        color.setAttribute('color', fill)
+                    }
+                } catch (e) {
+                    console.error(`svg ${this.name} 不存在`)
+                }
+            },
+            clean() {
+                if (this.type !== 'svg') return
+                if (this.svgParent) {
+                    for (const k of Object.keys(this.clonedSvg)) {
+                        try {
+                            this.svgParent.removeChild(this.clonedSvg[k])
+                        } catch (e) {
+                        }
                     }
                 }
-                for (const color of colors) {
-                    color.setAttribute('color', fill)
-                }
-            } catch (e) {
-                console.error(`svg ${ this.name } 不存在`)
+                this.clonedSvg = {}
             }
-        },
-        clean () {
-            if (this.type !== 'svg') return
-            if (this.svgParent) {
-                for (const k of Object.keys(this.clonedSvg)) {
-                    try {
-                        this.svgParent.appendChild(this.clonedSvg[k])
-                    } catch (e) {}
-                }
-            }
-            this.clonedSvg = {}
         }
     }
-}
 </script>
 
 <style module lang="scss">
-  .pl-svg {
-  }
+    .pl-svg {
+    }
 </style>
