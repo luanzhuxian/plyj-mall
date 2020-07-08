529b75076b57a0a260b3053138b1ef1d793e2dce
lzx <luanzhuxian@hotmail.com>
2020-07-08 13:21:21 +0800

    refactor: navbar: 替换图标

--- src/components/common/Navbar.vue
+++ src/components/common/Navbar.vue
@@ -1,62 +1,48 @@
 <template>
     <nav :class="[$style.navbar, $style[skinClassNameMap[skinId]]]">
+        <!-- 首次进入商城，请求未返回时，skinId 为 null，此时不显示图标，避免接口返回后切换皮肤图标，视觉体验不好 -->
         <!-- 首页 -->
         <router-link
-            :class="{
-                [$style.route]: true,
-                [$style.active]: homeActive
-            }"
+            v-if="skinId !== null"
+            :class="[$style.route, homeActive ? $style.active : '']"
             tag="div"
             :to="{ name: 'Home' }"
         >
-
-            <template v-if="isSkinShow">
-                <img :src="homeActive ? skinNavMap[skinId].homeActive : skinNavMap[skinId].home">
-                <div :class="$style.tabName">首页</div>
-            </template>
-            <template v-else-if="skinId !== null">
-                <img
-                    :class="$style.icon"
-                    src="https://mallcdn.youpenglai.com/static/mall/icons/olds/find.png"
-                    style="width: 5.86vw; height: 9.06vw;"
-                    alt=""
-                >
-                <img
-                    :class="{ [$style.icon]: true, [$style.active]: true, [$style.show]: homeActive }"
-                    src="https://mallcdn.youpenglai.com/static/mall/icons/olds/find (1).png"
-                    style="width: 5.86vw; height: 9.06vw;"
-                    alt=""
-                >
-            </template>
+            <img
+                v-if="isSkinShow"
+                v-imgError
+                :src="homeActive ? skinNavMap[skinId].homeActive : skinNavMap[skinId].home"
+            >
+            <img
+                v-else
+                :class="$style.icon"
+                :src="homeActive ? iconMap.homeActive : iconMap.home"
+                style="width: 6.2vw; height: 5.73vw;"
+            >
+            <div :class="$style.tabName">首页</div>
         </router-link>
+
         <!-- 分类 -->
         <router-link
-            :class="{
-                [$style.route]: true,
-                [$style.active]: classifyActive
-            }"
+            v-if="skinId !== null"
+            :class="[$style.route, classifyActive ? $style.active : '']"
             tag="div"
             :to="{ name: 'Classify' }"
         >
-            <template v-if="isSkinShow">
-                <img :src="classifyActive ? skinNavMap[skinId].classifyActive : skinNavMap[skinId].classify">
-                <div :class="$style.tabName">分类</div>
-            </template>
-            <template v-else-if="skinId !== null">
-                <img
-                    :class="$style.icon"
-                    src="https://mallcdn.youpenglai.com/static/mall/icons/olds/classify (1).png"
-                    style="width: 5.86vw; height: 9.06vw;"
-                    alt=""
-                >
-                <img
-                    :class="{ [$style.icon]: true, [$style.active]: true, [$style.show]: classifyActive }"
-                    src="https://mallcdn.youpenglai.com/static/mall/icons/olds/classify.png"
-                    style="width: 5.86vw; height: 9.06vw;"
-                    alt=""
-                >
-            </template>
+            <img
+                v-if="isSkinShow"
+                v-imgError
+                :src="classifyActive ? skinNavMap[skinId].classifyActive : skinNavMap[skinId].classify"
+            >
+            <img
+                v-else
+                :class="$style.icon"
+                :src="classifyActive ? iconMap.classifyActive : iconMap.classify"
+                style="width: 5.33vw; height: 5.73vw;"
+            >
+            <div :class="$style.tabName">分类</div>
         </router-link>
+
         <!-- 中间的活动button -->
         <router-link
             v-if="isActivityAuth"
@@ -80,62 +66,47 @@
                 src="https://mallcdn.youpenglai.com/static/admall/mall-management/dragon-gate/main-icon.png"
             >
         </router-link>
+
         <!-- 购物车 -->
         <router-link
-            :class="{
-                [$style.route]: true,
-                [$style.active]: shoppingChartActive
-            }"
+            v-if="skinId !== null"
+            :class="[$style.route, shoppingCartActive ? $style.active : '']"
             tag="div"
             :to="{ name: 'ShoppingCart' }"
         >
-            <template v-if="isSkinShow">
-                <img v-imgError :src="shoppingChartActive ? skinNavMap[skinId].shoppingChartActive : skinNavMap[skinId].shoppingChart">
-                <div :class="$style.tabName">购物车</div>
-            </template>
-            <template v-else-if="skinId !== null">
-                <img
-                    :class="$style.icon"
-                    src="https://mallcdn.youpenglai.com/static/mall/icons/olds/cart (1).png"
-                    style="width: 8.53vw; height: 10.13vw;"
-                    alt=""
-                >
-                <img
-                    :class="{ [$style.icon]: true, [$style.active]: true, [$style.show]: shoppingChartActive }"
-                    src="https://mallcdn.youpenglai.com/static/mall/icons/olds/cart.png"
-                    style="width: 8.53vw; height: 10.13vw;"
-                    alt=""
-                >
-            </template>
-
+            <img
+                v-if="isSkinShow"
+                v-imgError
+                :src="shoppingCartActive ? skinNavMap[skinId].shoppingCartActive : skinNavMap[skinId].shoppingCart"
+            >
+            <img
+                v-else
+                :class="$style.icon"
+                :src="shoppingCartActive ? iconMap.shoppingCartActive : iconMap.shoppingCart"
+                style="width: 5.84vw; height: 6vw;"
+            >
+            <div :class="$style.tabName">购物车</div>
         </router-link>
+
         <!-- 我的 -->
         <router-link
-            :class="{
-                [$style.route]: true,
-                [$style.active]: myActive
-            }"
+            v-if="skinId !== null"
+            :class="[$style.route, myActive ? $style.active : '']"
             tag="div"
             :to="{ name: 'My' }"
         >
-            <template v-if="isSkinShow">
-                <img v-imgError :src="myActive ? skinNavMap[skinId].myActive : skinNavMap[skinId].my">
-                <div :class="$style.tabName">我的</div>
-            </template>
-            <template v-else-if="skinId !== null">
-                <img
-                    :class="$style.icon"
-                    src="https://mallcdn.youpenglai.com/static/mall/icons/olds/my.png"
-                    style="width: 5.87vw; height: 9.87vw;"
-                    alt=""
-                >
-                <img
-                    :class="{ [$style.icon]: true, [$style.active]: true, [$style.show]: myActive }"
-                    src="https://mallcdn.youpenglai.com/static/mall/icons/olds/my (1).png"
-                    style="width: 5.87vw; height: 9.87vw;"
-                    alt=""
-                >
-            </template>
+            <img
+                v-if="isSkinShow"
+                v-imgError
+                :src="myActive ? skinNavMap[skinId].myActive : skinNavMap[skinId].my"
+            >
+            <img
+                v-else
+                :class="$style.icon"
+                :src="myActive ? iconMap.myActive : iconMap.my"
+                style="width: 5.33vw; height: 6.4vw;"
+            >
+            <div :class="$style.tabName">我的</div>
             <div v-if="noticeStatus === 2" :class="$style.alertMessage" />
         </router-link>
     </nav>
@@ -146,11 +117,22 @@ import { mapGetters, mapActions } from 'vuex'
 import { Get_ADUIT_NOTICE } from '../../store/mutation-type'
 import { skinClassNameMap, skinNavMap } from '../../views/home/skin/map'
 
+const iconMap = {
+    home: 'https://mallcdn.youpenglai.com/static/mall/icons/navbar/home.png',
+    homeActive: 'https://mallcdn.youpenglai.com/static/mall/icons/navbar/home-active.png',
+    classify: 'https://mallcdn.youpenglai.com/static/mall/icons/navbar/classify.png',
+    classifyActive: 'https://mallcdn.youpenglai.com/static/mall/icons/navbar/classify-active.png',
+    shoppingCart: 'https://mallcdn.youpenglai.com/static/mall/icons/navbar/shopping-cart.png',
+    shoppingCartActive: 'https://mallcdn.youpenglai.com/static/mall/icons/navbar/shopping-cart-active.png',
+    my: 'https://mallcdn.youpenglai.com/static/mall/icons/navbar/my.png',
+    myActive: 'https://mallcdn.youpenglai.com/static/mall/icons/navbar/my-active.png'
+}
+
 export default {
     name: 'Navbar',
     data () {
-        this.timer = 0
         return {
+            timer: 0,
             showNavbar: [
                 'Home',
                 'Classify',
@@ -159,7 +141,8 @@ export default {
                 'WhatsHelper'
             ],
             skinClassNameMap,
-            skinNavMap
+            skinNavMap,
+            iconMap
         }
     },
     computed: {
@@ -170,7 +153,7 @@ export default {
         classifyActive () {
             return this.$route.matched.some(val => val.name === 'Classify')
         },
-        shoppingChartActive () {
+        shoppingCartActive () {
             return this.$route.path.indexOf('cart') > -1
         },
         myActive () {
@@ -222,34 +205,29 @@ export default {
     position: relative;
     display: inline-flex;
     flex-direction: column;
-    justify-content: center;
+    justify-content: space-between;
     align-items: center;
-    font-size: 20px;
+    box-sizing: border-box;
+    padding: 8px 0 4px;
     height: 88px;
-    .alertMessage {
-        background-color: #d2524c;
-        width: 10px;
-        height: 10px;
-        border-radius: 50%;
-        position: absolute;
-        top: 10px;
-        right: 60px;
-    }
+    font-size: 18px;
+    color: #666666;
     > .icon {
         width: 71px;
         height: 62px;
         &.active {
-            position: absolute;
-            top: 50%;
-            left: 50%;
-            opacity: 0;
-            transform: translate(-50%, -50%);
-            transition: opacity .3s;
-            &.show {
-                opacity: 1;
-            }
+            color: #F2B036;
         }
     }
+}
+.alert-message {
+    background-color: #d2524c;
+    width: 10px;
+    height: 10px;
+    border-radius: 50%;
+    position: absolute;
+    top: 10px;
+    right: 60px;
 }
 .icon-d12 {
     box-sizing: content-box;
