36da56d8c061c7d42b6f8f5303aa5b611b9bf740
lishaobos <www.long126@qq.com>
2020-07-13 16:52:43 +0800

    fix: 直播答题

--- src/views/live/components/Live-Interact/Answer.vue
+++ src/views/live/components/Live-Interact/Answer.vue
@@ -1,12 +1,12 @@
 <template>
-    <div :class="[$style.lump, $style.answer]">
+    <div :class="[$style.lump, $style.answer]" @click="showData(data)">
         <img :class="$style.icon" src="https://mallcdn.youpenglai.com/static/mall/icons/2.11.0/答题.png" alt="">
         <span :class="$style.title">答题</span>
     </div>
 </template>
 
 <script>
-import RadioGroup from './components/Radio-Group'
+import CheckGroup from './components/Check-Group'
 
 export default {
     name: 'Answer',
@@ -19,65 +19,145 @@ export default {
         socket: {
             type: Object,
             default: null
+        },
+        channelId: {
+            type: [String, Number],
+            default: ''
+        },
+        userName: {
+            type: String,
+            default: ''
+        },
+        userId: {
+            type: String,
+            default: ''
         }
     },
     data () {
         return {
-            answer: ''
+            singleAnswer: '',
+            multipleAnswer: [],
+            // 提交答案集合
+            answers: new Map(),
+            data: null
         }
-    },
-    created () {
-        this.$propmt({
-            hasCancelButton: false,
-            hasDefaultInput: false,
-            closeOnClickMask: false,
-            useDangersHtml: true,
-            message: '答题',
-            viceMessage: '答题',
-            slot: this.$createElement(RadioGroup, {
-                props: {
-                    value: this.answer,
-                    data: ['A', 'B', 'C']
-                },
-                on: {
-                    change: val => {
-                        this.answer = val
-                    }
-                }
-            }),
-            customValidate: () => this.answer
-        })
     },
     methods: {
         init () {
-            const { EVENTS: { GET_TEST_QUESTION_CONTENT, STOP_TEST_QUESTION } } = window.PolyvLiveSdk
-            this.liveSdk.on(GET_TEST_QUESTION_CONTENT, this.receive)
-            this.liveSdk.on(STOP_TEST_QUESTION, this.end)
-            this.liveSdk.on(STOP_TEST_QUESTION, this.getResult)
+            const { EVENTS: { GET_TEST_QUESTION_CONTENT, GET_TEST_QUESTION_RESULT } } = window.PolyvLiveSdk
+            this.liveSdk.on(GET_TEST_QUESTION_CONTENT, (event, data) => this.showData(data))
+            this.liveSdk.on(GET_TEST_QUESTION_RESULT, (event, data) => this.getResult(data))
         },
-        receive (event, data) {
+        getOptions (data) {
+            let index = 1
+            let key = `option${ index }`
+            const options = []
+            const choice = ['', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
+            while (data.hasOwnProperty(key) && data[key] !== null) {
+                options.push({
+                    label: data[key],
+                    value: choice[index]
+                })
+                key = `option${ ++index }`
+            }
+            return options
+        },
+        async showData (data) {
+            if (!data) {
+                this.$alert({
+                    message: '答题',
+                    viceMessage: '主播暂未发起答题~'
+                })
+                return
+            }
             // itemType 答题类型，1表示快速问答，0表示答题卡
-            // type 题目类型，R表示单选，C表示多选，S表示评分（快速问答无此类型题目）
-            console.log(data)
+            // type 题目类型，R表示单选，C表示多选，V表示投票，S表示评分（快速问答无此类型题目）
+            const { itemType, type, name, questionId } = data
+            const isQuick = itemType === 1
+            const isMultiple = type === 'C'
+            this.data = data
+
+            const customValidate = () => {
+                if (isMultiple) {
+                    if (!this.multipleAnswer.length) this.$warning('请选择答案')
+                    return this.multipleAnswer.length
+                }
+
+                if (!this.singleAnswer) this.$warning('请选择答案')
+                return this.singleAnswer
+            }
+
+            const change = val => {
+                isMultiple ? this.multipleAnswer = val : this.singleAnswer = val
+            }
+
+            await this.$propmt({
+                hasCancelButton: false,
+                hasDefaultInput: false,
+                useDangersHtml: true,
+                message: '答题',
+                viceMessage: `【${ isMultiple ? '多选' : '单选' }】 ${ isQuick ? '请选择你的答案' : name }`,
+                confirmText: '提交答案',
+                slot: this.$createElement(CheckGroup, {
+                    props: {
+                        isResult: false,
+                        isSingle: !isMultiple,
+                        isQuick,
+                        value: isMultiple ? this.multipleAnswer : this.singleAnswer,
+                        data: this.getOptions(data)
+                    },
+                    on: {
+                        change
+                    }
+                }),
+                customValidate
+            })
+
+            this.answers.set(questionId, isMultiple ? this.multipleAnswer : this.singleAnswer)
+            const answer = isMultiple ? this.multipleAnswer.join('') : this.singleAnswer
+            this.send(answer, data.questionId)
+            this.clear()
         },
-        getResult (event, data) {
-            console.log(data)
+        getResult (data) {
+            const { itemType, type, name, questionId, answer: rightAnswers } = data.content
+            const isQuick = itemType === 1
+            const isMultiple = type === 'C'
+            const answer = this.answers.get(questionId)
+
+            this.$propmt({
+                hasCancelButton: false,
+                hasDefaultInput: false,
+                useDangersHtml: true,
+                message: '答题结果',
+                viceMessage: name,
+                slot: this.$createElement(CheckGroup, {
+                    props: {
+                        rightAnswers: rightAnswers.split(''),
+                        isResult: true,
+                        isSingle: !isMultiple,
+                        isQuick,
+                        value: answer,
+                        data: this.getOptions(data.content)
+                    }
+                })
+            })
         },
-        send () {
-            this.socket.emit('message', JSON.stringify({
+        send (option, questionId) {
+            const { channelId, userName: nick, userId } = this
+            const data = {
                 EVENT: 'ANSWER_TEST_QUESTION',
-                roomId: ``,
-                nick: '',
-                userId: '',
-                option: '',
-                questionId: ''
-            }))
+                roomId: `${ channelId }`,
+                nick,
+                userId,
+                option,
+                questionId
+            }
+            this.socket.emit('message', JSON.stringify(data))
         },
-        end (event, data) {
-
-        },
-        createElement () {
-
+        clear () {
+            this.singleAnswer = ''
+            this.multipleAnswer = []
+            this.data = null
         }
     }
 }
