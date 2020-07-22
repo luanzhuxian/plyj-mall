85da53c1f733c8e4d13210d4d37987b5051a960e
EchoPa <cuilulu4043@126.com>
2020-07-22 09:33:05 +0800

    fix: (订单) #1837 【c端】预购待付尾款时，核销码没隐藏干净

--- src/views/my/order/Detail.vue
+++ src/views/my/order/Detail.vue
@@ -263,7 +263,7 @@
                 <pl-fields
                     size="middle"
                     :text="`学员信息${i + 1}`"
-                    :right-text="!isRefundsFinalStage && i < redeemCodeModels.length && redeemCodeModels[i].code ? `核销码：${localSeparator(redeemCodeModels[i].code,' ', 4)}`: ''"
+                    :right-text="!isRefundsFinalStage && ([orderStatuskeyMap.WAIT_RECEIVE, orderStatuskeyMap.FINISHED].includes(detail.status) || (detail.paymentMethod === 1 && orderStatuskeyMap.WAIT_PAY_TAIL_MONEY === detail.status)) && i < redeemCodeModels.length && redeemCodeModels[i].code ? `核销码：${localSeparator(redeemCodeModels[i].code,' ', 4)}`: ''"
                     icon="icon-name-card"
                     title-color="#F2B036"
                     :icon-width="40"
