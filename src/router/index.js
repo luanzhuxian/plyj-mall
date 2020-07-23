dc4742c2a0419f8f5e80f981395f33694fad9871
Tesla <547249523@qq.com>
2020-07-23 14:05:44 +0800

    fix: 优化代码

--- src/router/index.js
+++ src/router/index.js
@@ -28,20 +28,20 @@ Vue.use(Router)
  * 有时会有错误抛出，但是并不会影响正常跳转
  * 为了能避免错误的打印，以下式vue-router作者给出的解决方案
  * 同时，在这里可以拦截push和replace方法，为自定义路由参数提供了无限的可能性
- * 例如：下面就是为每次路由跳转提供了一个noCache的参数，值为时间戳，以防止微信的缓存和CDN缓存
- * 由于noCache是可选的，所有有没有都不影响路由的正常访问
+ * 例如：下面就是为每次路由跳转提供了一个t的参数，值为时间戳，以防止微信的缓存和CDN缓存
+ * 由于t是可选的，所有有没有都不影响路由的正常访问
  */
 const originalPush = Router.prototype.push
 const originalReplace = Router.prototype.replace
 Router.prototype.push = function push (location, onResolve, onReject) {
     if (typeof location === 'object') {
-        const noCache = `${ Date.now() }`
+        const t = `${ Date.now() }`
         if (location.query) {
-            location.query.noCache = noCache
+            location.query.t = t
         } else {
             Object.assign(location, {
                 query: {
-                    noCache
+                    t
                 }
             })
         }
@@ -51,13 +51,13 @@ Router.prototype.push = function push (location, onResolve, onReject) {
 }
 Router.prototype.replace = function replace (location, onResolve, onReject) {
     if (typeof location === 'object') {
-        const noCache = `${ Date.now() }`
+        const t = `${ Date.now() }`
         if (location.query) {
-            location.query.noCache = noCache
+            location.query.t = t
         } else {
             Object.assign(location, {
                 query: {
-                    noCache
+                    t
                 }
             })
         }
@@ -116,14 +116,6 @@ const allRoutes = [
     ...OnlineClassroom,
     ...LongmenFestival
 ]
-
-// 为每个路由配置可选参数noCache
-// for (let route of allRoutes) {
-//   const paths = route.path.split('/')
-//   if (!paths[0]) {
-//     // route.path += '/:noCache?'
-//   }
-// }
 
 export const router = new Router({
     mode: 'history',
