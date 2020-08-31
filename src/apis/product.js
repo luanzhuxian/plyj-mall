026e9d74bd400987e10c331fa99f74045ba288fd
Tesla <547249523@qq.com>
2020-08-31 08:57:07 +0800

    fix: 删除多余接口

--- src/apis/product.js
+++ src/apis/product.js
@@ -3,9 +3,6 @@ import { axios } from '../assets/js/axios'
 // 获取商品详情
 export const getProductDetail = (goodsId, productStatus) => axios.get(`/apis/v1/product/detail/${ goodsId }?source=1&productStatus=${ productStatus }`)
 
-// 创建分享连接
-export const createBrokerShare = productId => axios.post(`/apis/v1/agent/user/share/record/create`, { productId })
-
 // C 端用户进入商品详情优惠券列表
 export const getCouponInDetail = productId => axios.get(`/apis/v1/coupon/list/productDetail?productId=${ productId }`)
 
