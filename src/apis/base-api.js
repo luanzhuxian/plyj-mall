6c29e63256aa9f4d13a93caf1a6d2357c2aa5dd3
Tesla <547249523@qq.com>
2020-09-22 10:15:12 +0800

    fix: 修改获取商品信息接口

--- src/apis/base-api.js
+++ src/apis/base-api.js
@@ -10,7 +10,7 @@ export const sendCode = (smsType, mobile) => axios.post(`/apis/v1/systemctl/sms/
 export const checkMobileCode = (smsType, mobile, verifyCode) => axios.get(`/apis/v1/systemctl/sms/${ smsType }/verify/${ mobile }/${ verifyCode }/verification`)
 
 // 根据域名获取商城id
-export const getMallInfo = domainName => axios.get(`/apis/v1/base/baseMall/query/appid?domainName=${ domainName }`)
+export const getMallInfo = domainName => axios.get(`/apis/v2/mall/query/appid?domainName=${ domainName }`)
 
 export const getOpenId = (appid, code) => axios.post(`/apis/v1/base/baseUserInfo/${ appid }/greet?code=${ code }`)
 
