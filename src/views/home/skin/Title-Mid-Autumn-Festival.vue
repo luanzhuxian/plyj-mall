1b617fe1bf57063d2f39b3a28760246734491ede
lzx <luanzhuxian@hotmail.com>
2020-09-25 19:05:18 +0800

    feat: 新增皮肤

---
+++ src/views/home/skin/Title-Mid-Autumn-Festival.vue
@@ -0,0 +1,43 @@
+<template>
+    <div :class="$style.titleMidAutumnFestival">
+        <div :class="$style.title" v-text="data" />
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'TitleAutumnFestival',
+    props: {
+        data: {
+            type: String,
+            default: ''
+        }
+    },
+    data () {
+        return {}
+    }
+}
+</script>
+
+<style lang="scss" module>
+.title-mid-autumn-festival {
+    position: relative;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 114px;
+    background: url('https://mallcdn.youpenglai.com/static/admall/skin/mid-autumn-festival/title.png') no-repeat center;
+    background-size: auto 100%;
+    .title {
+        position: relative;
+        top: 10px;
+        left: 30px;
+        max-width: 200px;
+        text-align: center;
+        font-size: 36px;
+        font-family: Microsoft YaHei;
+        color:#D96B20;
+    }
+}
+
+</style>
---
+++ src/views/home/skin/Title-National-Day.vue
@@ -0,0 +1,43 @@
+<template>
+    <div :class="$style.titleNationalDay">
+        <div :class="$style.title" v-text="data" />
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'TitleNationalDay',
+    props: {
+        data: {
+            type: String,
+            default: ''
+        }
+    },
+    data () {
+        return {}
+    }
+}
+</script>
+
+<style lang="scss" module>
+.title-national-day {
+    position: relative;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100px;
+    background: url('https://mallcdn.youpenglai.com/static/admall/skin/national-day/title.png') no-repeat center;
+    background-size: 100%;
+    .title {
+        position: relative;
+        top: -10px;
+        max-width: 400px;
+        text-align: center;
+        font-size: 40px;
+        font-family: Microsoft YaHei;
+        font-weight: bold;
+        color: #BA0909;
+    }
+}
+
+</style>
