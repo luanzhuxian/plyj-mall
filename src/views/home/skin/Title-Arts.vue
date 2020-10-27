bd7434382055b06d5c39a33100cfbf35ebd031f4
lzx <luanzhuxian@hotmail.com>
2020-10-27 18:17:22 +0800

    feat: 新增双十二4套皮肤

---
+++ src/views/home/skin/Title-Arts.vue
@@ -0,0 +1,42 @@
+<template>
+    <div :class="$style.titleArts">
+        <div :class="$style.title" v-text="data" />
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'TitleArts',
+    props: {
+        data: {
+            type: String,
+            default: ''
+        }
+    },
+    data () {
+        return {}
+    }
+}
+</script>
+
+<style lang="scss" module>
+.title-arts {
+    position: relative;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100px;
+    background: url('https://mallcdn.youpenglai.com/static/admall/skin/art/title.png') no-repeat center;
+    background-size: auto 100%;
+    .title {
+        position: relative;
+        left: 4px;
+        max-width: 200px;
+        text-align: center;
+        font-size: 40px;
+        font-family: Microsoft YaHei;
+        color: #FFFFFF;
+    }
+}
+
+</style>
---
+++ src/views/home/skin/Title-Sports.vue
@@ -0,0 +1,42 @@
+<template>
+    <div :class="$style.titleSports">
+        <div :class="$style.title" v-text="data" />
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'TitleSports',
+    props: {
+        data: {
+            type: String,
+            default: ''
+        }
+    },
+    data () {
+        return {}
+    }
+}
+</script>
+
+<style lang="scss" module>
+.title-sports {
+    position: relative;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 88px;
+    background: url('https://mallcdn.youpenglai.com/static/admall/skin/sports/title.png') no-repeat center;
+    background-size: auto 100%;
+    .title {
+        position: relative;
+        left: 10px;
+        max-width: 200px;
+        text-align: center;
+        font-size: 40px;
+        font-family: Microsoft YaHei;
+        color:#FFFFFF;
+    }
+}
+
+</style>
