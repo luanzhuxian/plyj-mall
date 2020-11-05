1b9989c0c0a90bfe3f238d20ed257b63fcd98005
matthew <527692503@qq.com>
2020-11-05 16:07:13 +0800

    fix: 修改v2

--- src/apis/money.js
+++ src/apis/money.js
@@ -1,13 +1,13 @@
 import axios from 'axios'
 
 // 分页查询提现记录
-export const getWithdrawDeposit = params => axios.get(`/apis/v1/agent/user/withdraw/deposit/page`, { params })
+export const getWithdrawDeposit = params => axios.get(`/apis/v2/agent/user/withdraw/deposit/page`, { params })
 
 // 分页查询当前登录用户提现记录
-export const getWithdrawDepositOfUser = params => axios.get(`/apis/v1/agent/user/withdraw/deposit/page/user`, { params })
+export const getWithdrawDepositOfUser = params => axios.get(`/apis/v2/agent/user/withdraw/deposit/page/user`, { params })
 
 // 提现详情
-export const getWithdrawDepositDetail = sequenceNbr => axios.get(`/apis/v1/agent/user/withdraw/deposit/${ sequenceNbr }`)
+export const getWithdrawDepositDetail = sequenceNbr => axios.get(`/apis/v2/agent/user/withdraw/deposit/${ sequenceNbr }`)
 
 // 提现
 export const withdrawDeposit = data => axios.post('/apis/v2/agent/user/withdraw/deposit', data)
@@ -16,4 +16,4 @@ export const withdrawDeposit = data => axios.post('/apis/v2/agent/user/withdraw/
 export const getBrokerageList = params => axios.get(`/apis/v1/agent/user/share/price/record/page/user`, { params })
 
 // 获取提现明细个状态的数量
-export const getDepositCount = () => axios.get(`/apis/v1/agent/user/withdraw/deposit/statistics`)
+export const getDepositCount = () => axios.get(`/apis/v2/agent/user/withdraw/deposit/statistics`)
