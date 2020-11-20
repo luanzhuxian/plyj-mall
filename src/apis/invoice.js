bcb6a7511b83ff06c0e4462122cc7d019899211d
Tesla <547249523@qq.com>
2020-11-20 19:17:23 +0800

    fix: 修改发票接口

--- src/apis/invoice.js
+++ src/apis/invoice.js
@@ -4,25 +4,25 @@ import axios from 'axios'
 * 新增发票信息
 * @data object {userId, entName, tin}
 * */
-export const addInvoice = data => axios.post('/apis/v1/base/baseUserInvoice/add', data)
+export const addInvoice = data => axios.post('/apis/v2/base/baseUserInvoice/add', data)
 
 /*
 * 获取发票信息列表
 * @userId string
 * */
-export const getInvoiceList = (userId, id = '') => axios.post('/apis/v1/base/baseUserInvoice/select', { userId, id })
+export const getInvoiceList = (userId, id = '') => axios.post('/apis/v2/base/baseUserInvoice/select', { userId, id })
 
 /*
 * 修改发票信息
 * @userId string
 * */
-export const updateInvoice = data => axios.post('/apis/v1/base/baseUserInvoice/update', data)
+export const updateInvoice = data => axios.post('/apis/v2/base/baseUserInvoice/update', data)
 
 /*
 * 删除
 * @userId string
 * */
-export const removeInvoice = id => axios.post('/apis/v1/base/baseUserInvoice/delete', { id })
+export const removeInvoice = id => axios.post('/apis/v2/base/baseUserInvoice/delete', { id })
 
 /*
 * 申请发票
