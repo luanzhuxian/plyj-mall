f5e933db5de63d92638992298dde71c9171aa9e1
lzx <luanzhuxian@hotmail.com>
2020-12-02 17:50:49 +0800

    fix: 福利红包 修改弹幕文案

--- src/views/marketing-activity/red-package/utils.js
+++ src/views/marketing-activity/red-package/utils.js
@@ -15,7 +15,7 @@ export const getDuration = (status, receiveStartTime, receiveEndTime) => {
 // 获取福利红包弹幕模板
 export const getBulletTemplate = (bullet, vm) => {
     const { userImage, userName, mobile, time, msg } = bullet
-    const message = `${ userName.slice(0, 1) }** ${ String(mobile).slice(0, 3) }****${ String(mobile).slice(-4) } ${ time }领取${ msg }的储备金`
+    const message = `${ userName.slice(0, 1) }** ${ String(mobile).slice(0, 3) }****${ String(mobile).slice(-4) } ${ time }领取${ msg }的福利红包`
     const template = `
         <div class="my-bullet">
             <div class="my-bullet__avatar">
