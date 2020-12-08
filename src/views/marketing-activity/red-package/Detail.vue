083236c47ea4828384f72e645f3468a2d770fb9c
Tesla <547249523@qq.com>
2020-12-08 19:27:44 +0800

    fix: #1002543 红包购买传递分享id

--- src/views/marketing-activity/red-package/Detail.vue
+++ src/views/marketing-activity/red-package/Detail.vue
@@ -239,7 +239,7 @@ export default {
         }
     },
     computed: {
-        ...mapGetters(['userId', 'realName', 'userName', 'mobile', 'appId', 'mallUrl']),
+        ...mapGetters(['userId', 'realName', 'userName', 'mobile', 'appId', 'mallUrl', 'SHARE_ID']),
         isCountdownShow () {
             return this.status === 0 || this.status === 1
         },
@@ -386,7 +386,7 @@ export default {
                 }
 
                 this.submiting = true
-                await submitRedPackageOrder(this.activityId, this.form.count, this.userId)
+                await submitRedPackageOrder(this.activityId, this.form.count, this.SHARE_ID)
                 this.activity.userClaimed++
                 await this.showModel('success')
             } catch (error) {
