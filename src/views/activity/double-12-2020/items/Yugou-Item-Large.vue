535604b0463150f59b703b4979d600026dd47de6
lzx <luanzhuxian@hotmail.com>
2020-12-11 16:13:03 +0800

    fix: 店铺管理 修改预购

--- src/views/activity/double-12-2020/items/Yugou-Item-Large.vue
+++ src/views/activity/double-12-2020/items/Yugou-Item-Large.vue
@@ -9,7 +9,7 @@
             query: { currentProductStatus: 4 }
         }"
     >
-        <div :class="$style.imgWrapper">
+        <div :class="$style.imgWrapper" class="img-wrapper">
             <img :src="data.goodsInfo.productMainImage + '?x-oss-process=style/thum-middle'">
             <div :class="$style.bar">
                 <div :class="$style.rule">
