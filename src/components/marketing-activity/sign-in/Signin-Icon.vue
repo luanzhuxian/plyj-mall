35178fdedf5b3dcda21f0f71f168bd9dfbbf503b
wilson <411259243@qq.com>
2020-12-15 14:55:29 +0800

    feat: 打卡聪明年图标海报

--- src/components/marketing-activity/sign-in/Signin-Icon.vue
+++ src/components/marketing-activity/sign-in/Signin-Icon.vue
@@ -11,14 +11,15 @@
             <p :class="{'not-sign': !hasSignin}">{{ iconName }}</p>
         </div>
         <pl-mask :show.sync="show">
-            <img style="height: 70vh;" v-if="!loading" :src="bgi" alt="">
+            <img style="height: 70vh;" v-if="!loading" :src="poster" alt="">
             <pl-svg v-else class="loading" name="icon-loading" fill="#fff" height="45" />
         </pl-mask>
     </div>
 </template>
 
 <script>
-import { loadImage, createCanvas, generateQrcode } from '../../../assets/js/util'
+import { loadImage, createCanvas, generateQrcode, drawRoundRect, cutArcImage, createText } from '../../../assets/js/util'
+import { mapGetters } from 'vuex'
 export default {
     name: 'SigninIcon',
     data () {
@@ -63,6 +64,9 @@ export default {
             default: 2668
         }
     },
+    computed: {
+        ...mapGetters(['avatar', 'userName'])
+    },
     methods: {
         async clickHandler () {
             if (!this.detail.hasSignin) return
@@ -75,14 +79,40 @@ export default {
         },
         async createPoster () {
             const { cvs, ctx } = createCanvas(this.w, this.h)
+            // 白色背景
+            ctx.fillStyle = '#fff'
+            ctx.fillRect(0, 0, this.w, this.h)
+
+            // 绘制海报图片
             const bg = await loadImage(this.bgi)
             ctx.drawImage(bg, 0, 0, this.w, 2269)
+
+            // 绘制头像边框
+            drawRoundRect({ ctx, x: 102, y: 2349, width: 264, height: 264, radius: 132, strokeStyle: '#fc4e15' })
+
+            // 绘制圆形头像
+            let userImg = await loadImage(this.avatar)
+            userImg = await cutArcImage(userImg)
+            ctx.drawImage(userImg, 128, 2372, 215, 215)
+            // 绘制用户名、绘制文本
+            ctx.font = '46px Microsoft YaHei'
+            ctx.fillStyle = '#070707'
+            ctx.fillText(`${ this.userName }`, 393, 2425)
+            createText({ ctx, x: 393, y: 2499, text: '邀您一起打卡聪明年，领取奖励', lineHeight: 72, width: 320, lineNumber: 2 })
+
+            // 绘制二维码边框
+            drawRoundRect({ ctx, x: 793, y: 2349, width: 264, height: 264, radius: 28, strokeStyle: '#fc4e15', fillStyle: '#fff' })
             const qrcode = await generateQrcode({
                 size: 300,
                 text: location.href,
                 type: 'canvas'
             })
-            ctx.drawImage(qrcode, 522, 2300, 480, 480)
+            ctx.drawImage(qrcode, 822, 2374, 210, 210)
+
+            // 绘制文本
+            ctx.font = '46px Microsoft YaHei'
+            ctx.fillStyle = '#070707'
+            createText({ ctx, x: 1090, y: 2425, text: '长按识别或保存图片，分享给你的好友吧！', lineHeight: 72, width: 310, lineNumber: 3 })
             this.poster = cvs.toDataURL('image/jpeg', 0.8)
         }
     }
