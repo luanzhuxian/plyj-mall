3667f239e1de846a50cfabb87a3d64c22e5b1e63
Tesla <547249523@qq.com>
2020-12-20 12:28:04 +0800

    fix: 开启debug

--- src/assets/js/wechat/wechat-share.js
+++ src/assets/js/wechat/wechat-share.js
@@ -121,7 +121,7 @@ function getConfig (jsapi, appId, link) {
     const sign = `jsapi_ticket=${ jsapi }&noncestr=${ nonceStr }&timestamp=${ timestamp }&url=${ link }`
     const signature = new JsSHE(sign, 'TEXT').getHash('SHA-1', 'HEX')
     return {
-        debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+        debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
         appId, // 必填，公众号的唯一标识
         timestamp, // 必填，生成签名的时间戳
         nonceStr, // 必填，生成签名的随机串
